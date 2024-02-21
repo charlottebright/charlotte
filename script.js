37358788d0911962ed34180f40cae718ee5f5792
charlottebright <147007033+charlottebright@users.noreply.github.com>
2024-02-21 15:06:28 +0000

    basesite

---
+++ script.js
@@ -0,0 +1,8 @@
+let indexButton = document.querySelector(".indexButton");
+let header= document.querySelector("h1");
+
+
+indexButton.onclick = function() {
+        	header.innerHTML = "hello";
+ };
+    
